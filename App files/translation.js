0c302e733137283c1a66bfc8d38e79c30c6a3ac1
Kayleigh Soane <kayleigh.soane@students.plymouth.ac.uk>
2026-02-04 09:31:57 +0000

    Update translation.js

--- App files/translation.js
+++ App files/translation.js
@@ -113,7 +113,7 @@ function morsetoenglish() {
 }
 
 function otherlang() {
-    const APIkey = "APIKEY"; // Replace with your actual Google Cloud API key
+    const APIkey = "APIKEY"; // Replace with actual API key
     const APIlink = `https://translation.googleapis.com/language/translate/v2?key=${APIkey}`;
     
     fetch(APIlink, {
